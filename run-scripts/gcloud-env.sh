335aaa651860861d9034364bc5f14b67dc1e90b0
happystevehood <71417560+happystevehood@users.noreply.github.com>
2025-05-16 15:01:28 +0800

    Delete run-scripts/gcloud-env.sh
    Not needed

--- run-scripts/gcloud-env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Dont add these to git
-
-export SERVICE_ACCOUNT=951032250531-compute@developer.gserviceaccount.com
-export PROJECT_ID=redline-fitness-results
-
-#b'|\xd6%?\xc3\x8e\x87\xfc\xfd\xdc`\x9d\xb7\xfa\xe9\xc9p8u\xb5\x8eU\x8c\xec'
-#SteveisCool
